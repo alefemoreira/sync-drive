73259ad2b10b55b5963bc18051f9a8918e5e91bb
alefemoreira <delimaalefe@gmail.com>
2020-07-04 14:35:05 -0300

    refactor(login): returned to the old model
    References #3

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,27 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { IoLogoGoogle } from "react-icons/io";
-// import { v4 as uuid } from "uuid";
+import { MdAccountCircle, MdVpnKey } from "react-icons/md";
 
 import "./styles.css";
 
 const Login = () => {
   return (
     <div id="page-login">
-      <header>
-        <img
-          id="logo"
-          src={require("../../assets/logo.svg")}
-          alt="Sync Drive"
-        />
-      </header>
+      <div className="content">
+        <header>
+          <img
+            id="logo"
+            src={require("../../assets/logo.svg")}
+            alt="Sync Drive"
+          />
+        </header>
 
-      <main>
-        <Link to="/choose-folder">
-          <IoLogoGoogle size={32} />
-          <p>Entrar com o Google</p>
-        </Link>
-      </main>
+        <form>
+          <fieldset>
+            <div className="field-group">
+              <div className="field">
+                <div>
+                  <MdAccountCircle color="#282A36" size={40} />
+                </div>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="E-mail"
+                />
+              </div>
+
+              <div className="field">
+                <div>
+                  <MdVpnKey color="#282A36" size={40} />
+                </div>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+          </fieldset>
+          <Link to="/choose-folder">
+            <button type="submit">
+              <p>Entrar</p>
+            </button>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
