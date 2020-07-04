4dfef509143f6959198728d076f719e6f1d7ce2a
alefemoreira <delimaalefe@gmail.com>
2020-07-04 18:11:21 -0300

    fix(login page): login page back to only one button page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,56 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MdAccountCircle, MdVpnKey } from "react-icons/md";
+import { IoLogoGoogle } from "react-icons/io";
+import { signInWithGoogle } from "../../services/firebase";
 
 import "./styles.css";
 
 const Login = () => {
   return (
     <div id="page-login">
-      <div className="content">
-        <header>
-          <img
-            id="logo"
-            src={require("../../assets/logo.svg")}
-            alt="Sync Drive"
-          />
-        </header>
+      <header>
+        <img
+          id="logo"
+          src={require("../../assets/logo.svg")}
+          alt="Sync Drive"
+        />
+      </header>
 
-        <form>
-          <fieldset>
-            <div className="field-group">
-              <div className="field">
-                <div>
-                  <MdAccountCircle color="#282A36" size={40} />
-                </div>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="E-mail"
-                />
-              </div>
-
-              <div className="field">
-                <div>
-                  <MdVpnKey color="#282A36" size={40} />
-                </div>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
-          </fieldset>
-          <Link to="/choose-folder">
-            <button type="submit">
-              <p>Entrar</p>
-            </button>
-          </Link>
-        </form>
-      </div>
+      <main>
+        <a onClick={signInWithGoogle}>
+          <IoLogoGoogle size={32} />
+          <p>Entrar com o Google</p>
+        </a>
+      </main>
     </div>
   );
 };
